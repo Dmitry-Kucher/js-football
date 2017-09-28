670efac26523161b5e648269ca7057f2aa1c5e6a
Dima-macbook <dima@virtuman.com>
2017-09-28 12:25:34 +0300

    fixed package.json url, fixed problem with twice reload at webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -91,7 +91,7 @@ gulp.task('browserify', () => {
 
 // start webserver to test project
 gulp.task('webserver', ['watch'], () => {
-  gulp.src('./')
+  gulp.src('./app')
     .pipe(webserver({
       livereload: true,
       open: true,
