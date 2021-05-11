85cf0f294bf711e343c81a1ee73fea2472e44fdc
dkcuher <dmitry.a.kucher@gmail.com>
2021-05-11 21:07:47 +0300

    intial commit for boardgame.io generation of project

--- src/classes/PaperLine.js
+++
@@ -1,7 +0,0 @@
-export default class PaperLine {
-  constructor({ startPoint = { x: 0, y: 0 }, endPoint = { x: 50, y: 50 }, lineStyle } = {}) {
-    const line = new PIXI.Graphics();
-    line.drawLine({ startPoint, endPoint, lineStyle });
-    return line;
-  }
-}
